86b8bf800541ef5bfae603fa1a57da64b1f3483a
SaffronG <saffrongamer167@gmail.com>
2026-02-26 17:21:43 -0700

    added print arr func

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,3 +1,9 @@
 (* MAIN *)
-let tn = Utils.ns_of_c (2,2)
+let rec arr_print =
+    function
+    | [] -> ()
+    | (h1,h2)::t-> let () = (h1 |> string_of_int)^", "^(h2 |> string_of_int) |> print_endline in t |> arr_print
+let tn = Utils.deltas_of_b (2,2)
 let in_bounds = Utils.bind_ns 3 tn
+
+let () = arr_print tn
--- lib/utils.ml
+++ lib/utils.ml
@@ -9,3 +9,9 @@ let moore_nb = List.concat_map (fun x -> List.map (fun y -> (x,y)) deltas ) delt
 let deltas_of_b (cx, cy) = List.map (fun (dx, dy) -> (cx + dx, cy + dy)) moore_nb
 (** given a board bound b, return only cells within board *)
 let bind_ns b css = List.filter (fun (x, y) -> (x < b && x >= 0 && y < b && y >= 0)) css
+(** print the array using the tuple destructuring x, y *)
+let rec arr_print =
+    function
+    | [] -> ()
+    | (h1,h2)::t-> let () = (h1 |> string_of_int)^", "^(h2 |> string_of_int) |> print_endline in t |> arr_print
+
