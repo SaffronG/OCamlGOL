020eafeabf58fb0648eb1d3c42bf3d861c3fa9b1
SaffronG <saffrongamer167@gmail.com>
2026-02-27 19:50:53 -0700

    added tests, all passing except for correct_next_gen tests

--- _build/default/test/test_game_of_life.ml
+++ _build/default/test/test_game_of_life.ml
@@ -3,15 +3,21 @@
 (* Array Asserts  *)
 let () = assert (1 = 1)
 
-(* Assert Bind *)
-let test_board = Utils.{
-    cs = [|0;1;0;1|];
-    bound = 2
-}
 (* Test that unbound moores neighbors are produced *)
 let () = assert (Utils.deltas_of_b (1,1) = [(0,0);(0,1);(0,2);(1,0);(1,1);(1,2);(2,0);(2,1);(2,2)])
-(* Test that when bound it only produces cell indices within the given bound N *)
-let () = assert (Utils.deltas_of_b (0,0) |> (Utils.bind_ns test_board.bound) = [(0,0);(0,1);(1,0);(1,1)])
-let () = assert (Utils.deltas_of_b (1,1) |> (Utils.bind_ns test_board.bound) = [(0,0);(0,1);(1,0);(1,1)])
-let () = assert (Utils.deltas_of_b (2,2) |> (Utils.bind_ns test_board.bound) = [(1,1)])
-let () = assert (Utils.deltas_of_b (3,3) |> (Utils.bind_ns test_board.bound) = [])
+
+(* Test that the HashTable is correctly populated *)
+let () = assert (Utils.is_alive (Utils.next_gen (Hashtbl.create 1)) (0,0) = 0)
+let () = assert (Utils.is_alive (Utils.next_gen (Hashtbl.create 1)) (1,1) = 0)
+let () = assert (Utils.is_alive (Utils.next_gen (Hashtbl.create 1)) (2,2) = 0)
+
+(* Assert that you can initalize the HashTable with a glider *)
+let glider = Hashtbl.create 5
+let _ = List.map (fun (x, y) -> Hashtbl.add glider (x, y) 1) [(1, 0); (2, 1); (0, 2); (1, 2); (2, 2)]
+let glider_list = Hashtbl.fold (fun k _ acc -> k :: acc) glider []
+let () = assert (List.sort compare glider_list = [(0, 2); (1, 0); (1, 2); (2, 1); (2, 2)])
+
+(* Assert that the next generation of a glider is correct *)
+let next_glider = Utils.next_gen glider
+let next_glider_list = Hashtbl.fold (fun k _ acc -> k :: acc) next_glider []
+let () = assert (List.sort compare next_glider_list = [(0, 1); (1, 2); (2, 0); (2, 2)])
